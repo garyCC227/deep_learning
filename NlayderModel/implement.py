623ad865963bf1eb3676da655b5d15f2188b6f84
garyCC227 <z5163479@ad.unsw.edu.au>
2019-08-07 04:17:02 +1000

    cannot predict own image

--- NlayderModel/implement.py
+++ NlayderModel/implement.py
@@ -540,9 +540,11 @@ if __name__ == "__main__":
   my_label_y = [1]  # the true class of your image (1 -> cat, 0 -> non-cat)
 
   import imageio
+  from scipy import ndimage
+
   fname = "images/" + my_image
-  image = np.array(imageio.imread(fname, as_gray=False))
-  my_image = np.array(imageio.plugins.pillow.Image.fromarray(image).resize((num_px*num_px*3, 1)))
+  image = np.array(ndimage.imread(fname, flatten=False))
+  my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((num_px*num_px*3,1))
   my_image = my_image/255.
   my_predicted_image = NN_model.predict(my_image, my_label_y, parameters)
 
